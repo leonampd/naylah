f14455ac9aeae6ae3069bfe00bd06809fad09f57
Leonam Pereira Dias <leonam.pd@gmail.com>
2018-05-06 01:01:28 -0300

    webpack: change output directory for js and styles

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,16 @@ const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-  filename: '[name].css',
+  filename: (getPath) => {
+    return getPath('css/[name].css')
+  },
 });
 
 module.exports = {
   entry: './assets/src/js/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'assets/dist/js'),
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'assets/dist'),
   },
   module: {
     rules: [{
@@ -21,6 +23,7 @@ module.exports = {
           loader: 'sass-loader',
           options: {
             includePaths: [path.resolve(__dirname, 'assets/src/sass')],
+            publicPath: path.resolve(__dirname),
           },
         }],
         fallback: 'style-loader',
